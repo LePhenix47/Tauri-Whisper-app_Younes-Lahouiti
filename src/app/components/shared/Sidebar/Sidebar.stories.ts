82d0f80c17b2727f47fc12a5df21f7fe277dc289
Younes Lahouiti <loller763@gmail.com>
2025-10-11 11:36:44 +0200

    Removal of an import

--- src/app/components/shared/Sidebar/Sidebar.stories.ts
+++ src/app/components/shared/Sidebar/Sidebar.stories.ts
@@ -1,12 +1,10 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Sidebar } from "./Sidebar";
-import { withRouter } from "storybook-addon-react-router-v6";
 
 const meta: Meta<typeof Sidebar> = {
   title: "Shared/Sidebar",
   component: Sidebar,
   tags: ["autodocs"],
-  decorators: [withRouter],
   parameters: {
     layout: "fullscreen",
     docs: {
