692d1afeb0fd9b4f440ef16a160c45121068a2c1
Younes Lahouiti <loller763@gmail.com>
2025-11-16 20:59:34 +0100

    addiiotn of files to be ignored for Vite (Linux)

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,9 @@ export default defineConfig(({ mode }) => ({
   server: {
     port: 1420,
     strictPort: true,
+    watch: {
+      ignored: ["**/src-tauri/target/**"],
+    },
   },
   envPrefix: ["REACT_APP_", "VITE_", "TAURI_"],
   css: {
